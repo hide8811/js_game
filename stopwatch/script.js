d8d7e35774b5ad0149280f3eae990e327b32769e
hide8811 <ikuyedih18@gmail.com>
2021-08-11 18:36:57 +0900

    Add: [Stopwatch] ゲーム機能の追加

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,28 +1,34 @@
+const changeBtn = document.getElementById('change-btn');
+const changeStopwatchBtn = document.getElementById('change-stopwatch');
+const changeGameBtn = document.getElementById('change-game');
+const targetTimeArea = document.getElementById('target-time');
+const timeDiffArea = document.getElementById('time-diff');
 const display = document.getElementById('display');
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const restart = document.getElementById('restart');
-const reset = document.getElementById('reset');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const restartBtn = document.getElementById('restart');
+const resetBtn = document.getElementById('reset');
 
 let intervalTimer;
 let tmpTime;
 let status = 'zero';
+let mode = 'stopwatch';
 
 const changeToStartBtn = () => {
-  stop.classList.add('hide-btn');
-  restart.classList.add('hide-btn');
-  start.classList.remove('hide-btn');
+  stopBtn.classList.add('hide-btn');
+  restartBtn.classList.add('hide-btn');
+  startBtn.classList.remove('hide-btn');
 };
 
 const changeToStopBtn = () => {
-  start.classList.add('hide-btn');
-  restart.classList.add('hide-btn');
-  stop.classList.remove('hide-btn');
+  startBtn.classList.add('hide-btn');
+  restartBtn.classList.add('hide-btn');
+  stopBtn.classList.remove('hide-btn');
 };
 
 const changeToRestartBtn = () => {
-  stop.classList.add('hide-btn');
-  restart.classList.remove('hide-btn');
+  stopBtn.classList.add('hide-btn');
+  restartBtn.classList.remove('hide-btn');
 };
 
 const getTimes = (startTime) => {
@@ -50,11 +56,55 @@ const getTimes = (startTime) => {
   display.innerHTML = `${hourString}:${minutesString}:${secondString}.${millisecondsString}`;
 };
 
+const getGameTimes = (startTime) => {
+  const nowTime = new Date();
+  let time = nowTime - startTime;
+
+  tmpTime = time;
+
+  const milliseconds = time % 1000;
+  const millisecondsString = '0'.repeat(3 - String(milliseconds).length) + milliseconds;
+
+  time = Math.floor(time / 1000);
+
+  const second = time % 60;
+  const secondString = '0'.repeat(2 - String(second).length) + second;
+
+  display.innerHTML = `${secondString}.${millisecondsString}`;
+};
+
+const diffTime = () => {
+  let sign;
+  let diff;
+
+  if (tmpTime >= 10000) {
+    sign = '+';
+    diff = tmpTime - 10000;
+  } else {
+    sign = '-';
+    diff = 10000 - tmpTime;
+  }
+
+  const milliseconds = diff % 1000;
+  const millisecondsString = '0'.repeat(3 - String(milliseconds).length) + milliseconds;
+
+  diff = Math.floor(diff / 1000);
+
+  const secondString = String(diff % 60);
+
+  timeDiffArea.innerHTML = `${sign}${secondString}.${millisecondsString}`;
+};
+
 const startTimer = () => {
   if (status === 'zero') {
     const startTime = new Date();
 
-    intervalTimer = setInterval(getTimes, 1, startTime);
+    if (mode === 'stopwatch') {
+      intervalTimer = setInterval(getTimes, 1, startTime);
+    } else {
+      timeDiffArea.innerHTML = '';
+      intervalTimer = setInterval(getGameTimes, 1, startTime);
+    }
 
     changeToStopBtn();
 
@@ -66,9 +116,15 @@ const stopTimer = () => {
   if (status === 'move') {
     clearInterval(intervalTimer);
 
-    changeToRestartBtn();
+    if (mode === 'stopwatch') {
+      changeToRestartBtn();
+      status = 'stop';
+    } else {
+      diffTime();
 
-    status = 'stop';
+      changeToStartBtn();
+      status = 'zero';
+    }
   }
 };
 
@@ -86,17 +142,57 @@ const restartTimer = () => {
 };
 
 const resetTimer = () => {
-  if (status === 'stop' || status === 'move') {
+  clearInterval(intervalTimer);
+  display.innerHTML = (mode === 'stopwatch') ? '00:00:00.000' : '00.000';
+
+  timeDiffArea.innerHTML = '';
+
+  changeToStartBtn();
+
+  status = 'zero';
+};
+
+const changeStopwatchMode = () => {
+  if (mode === 'game') {
     clearInterval(intervalTimer);
+
     display.innerHTML = '00:00:00.000';
+    changeBtn.innerHTML = '<i class="fas fa-toggle-off"></i>';
+    timeDiffArea.innerHTML = '';
+    targetTimeArea.innerHTML = '';
 
     changeToStartBtn();
-
     status = 'zero';
+    mode = 'stopwatch';
   }
 };
 
-start.addEventListener('mousedown', startTimer);
-stop.addEventListener('mousedown', stopTimer);
-restart.addEventListener('mousedown', restartTimer);
-reset.addEventListener('mousedown', resetTimer);
+const changeGameMode = () => {
+  if (mode === 'stopwatch') {
+    clearInterval(intervalTimer);
+
+    display.innerHTML = '00.000';
+    changeBtn.innerHTML = '<i class="fas fa-toggle-on"></i>';
+    targetTimeArea.innerHTML = '10.000';
+
+    changeToStartBtn();
+    status = 'zero';
+    mode = 'game';
+  }
+};
+
+const modeChange = () => {
+  if (mode === 'stopwatch') {
+    changeGameMode();
+  } else {
+    changeStopwatchMode();
+  }
+};
+
+changeBtn.addEventListener('click', modeChange);
+changeStopwatchBtn.addEventListener('click', changeStopwatchMode);
+changeGameBtn.addEventListener('click', changeGameMode);
+startBtn.addEventListener('mousedown', startTimer);
+stopBtn.addEventListener('mousedown', stopTimer);
+restartBtn.addEventListener('mousedown', restartTimer);
+resetBtn.addEventListener('mousedown', resetTimer);
